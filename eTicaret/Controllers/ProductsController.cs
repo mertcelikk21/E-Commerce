0064732ff4f14dcceb5886f51145131fe797b31a
cmert <cmert@LAPTOP-BSENUEJ2>
2022-06-09 13:28:32 +0300

    Add project files.

---
+++ eTicaret/Controllers/ProductsController.cs
@@ -0,0 +1,37 @@
+﻿using eTicaret.Data.DataContext;
+using eTicaret.Data.DbModels;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eTicaret.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        private readonly StoreContext _context;
+
+        public ProductsController(StoreContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Product>>> GetProducts()
+        {
+            var data = await _context.Products.ToListAsync();
+            return data;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>> GetProduct(int id)
+        {
+            return await _context.Products.FindAsync(id);
+        }
+    }
+}
